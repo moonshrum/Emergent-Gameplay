9e316559c0ab7e1cd7c8dfececcdc6fa7e1caa27
inqblunt <jefflenane@gmail.com>
2019-09-24 14:29:03 +0200

    animals r here

--- Emergent Game/Emergent Gameplay Resit/Assets/Animal.cs
+++ Emergent Game/Emergent Gameplay Resit/Assets/Animal.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA.Input;
 
 public class Animal : MonoBehaviour
 {
     public CircleCollider2D SearchRadius;
     private Transform _target = null;
     public int AtkRange;
+    public float Speed;
+
+    private Vector3 _snapshot;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +20,57 @@ public class Animal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_target);
+        
         if (_target == null) return;
-        transform.LookAt(_target);
-        transform.position = Vector2.MoveTowards(transform.position, _target.position, 1);
+        //transform.LookAt(_target);
+        Vector2 heading = _target.position - transform.position;
+        heading.Normalize();
+        //Debug.Log(heading);
+        if (Vector2.Distance(_target.position, transform.position) > AtkRange)
+        {
+            Speed = 2;
+            heading = _snapshot - transform.position;
+            heading += AddNoiseOnAngle(0, 90);
+            heading.Normalize();
+        }
+        else
+        {
+            Speed = 5;
+            heading = _target.position - transform.position;
+        }
+        transform.Translate(heading * Speed * Time.deltaTime, Space.World);
 
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player") _target = other.transform;
+        _snapshot = other.transform.position;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "Player") _target = null;
+        if (other.transform == _target) _target = null;
+        Debug.Log("aaa");
+    }
+
+    Vector2 AddNoiseOnAngle(float min, float max)
+    {
+        // Find random angle between min & max inclusive
+        var xNoise = Random.Range(min, max);
+        var yNoise = Random.Range(min, max);
+
+        // Convert Angle to Vector2
+        var noise = new Vector2(
+            Mathf.Sin(2 * Mathf.PI * xNoise / 360),
+            Mathf.Sin(2 * Mathf.PI * yNoise / 360)
+        );
+        return noise;
+    }
+
+    void AcquireTarget(Transform enemy)
+    {
+
     }
 }
