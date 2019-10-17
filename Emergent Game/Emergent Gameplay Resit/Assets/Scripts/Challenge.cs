18cfab27aae6c56d28bdcf173ec2651ef4d4705b
Artem <artem.stolyga@gmail.com>
2019-10-17 12:18:12 +0200

    Added a tooltip for designers

--- Emergent Game/Emergent Gameplay Resit/Assets/Scripts/Challenge.cs
+++ Emergent Game/Emergent Gameplay Resit/Assets/Scripts/Challenge.cs
@@ -11,12 +11,14 @@ public class Challenge : ScriptableObject
     public string TextToAnnounce;
     [System.NonSerialized]
     public bool Complete;
+    [Tooltip("Write down the amount that the player needs to collect or kill")]
     public int AmountToCollectOrKill;
+    [Space(25f)]
     [Header("Fill in if the challenge requires the player to collect certain amount of a particular resource")]
     //public bool Collect;
+    public Resource.ResourceType TypeToCollect;
     [System.NonSerialized]
     public int AmountCollected;
-    public Resource.ResourceType TypeToCollect;
     [Space(25f)]
     [Header("Fill in if the challenge requires the player to kill someone")]
     //public int AmountToKill;
