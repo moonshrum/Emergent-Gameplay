d82e0ec9d82eebb1ff8da897d44414c073c8a187
Artem <artem.stolyga@gmail.com>
2019-11-02 17:32:40 +0100

    Changed script execution order

--- Emergent Game/Emergent Gameplay Resit/Assets/Scripts/Shop.cs
+++ Emergent Game/Emergent Gameplay Resit/Assets/Scripts/Shop.cs
@@ -178,7 +178,6 @@ public class Shop : MonoBehaviour
         {
             BuyButton =
         }*/
-        Debug.Log(SelectedItem.Name);
         BuyButton.sprite = CanCraftItem() ? BuyButtonSelected : BuyButtonDeSelected;
     }
 
